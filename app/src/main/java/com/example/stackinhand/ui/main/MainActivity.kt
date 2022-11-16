32d59f2febfee996c7df8717d45bd14078369f38
Apoorv Mishra <apoorvmishra@Apoorvs-MacBook-Pro.local>
2022-11-16 12:03:46 +0530

    UI Main Activity

--- app/src/main/java/com/example/stackinhand/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/stackinhand/ui/main/MainActivity.kt
@@ -3,10 +3,14 @@ package com.example.stackinhand.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.stackinhand.R
+import com.example.stackinhand.databinding.ActivityMainBinding
+import com.example.stackinhand.ui.base.BaseActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    override fun initViewBinding(): ActivityMainBinding {
+        return ActivityMainBinding.inflate(layoutInflater)
     }
+
+    override fun observeViewModel() { }
 }
